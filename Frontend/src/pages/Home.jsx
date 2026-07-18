7ea3608fc5a25392327e951e59b9d42965af22b1
Achutanand Gantayat <achutanandgantayat@gmail.com>
2026-07-18 23:28:03 +0530

    Basic Login UI done

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { signInWithPopup } from "firebase/auth"
+import { auth, googleProvider } from '../../utils/firebase'
+import api from '../../utils/axios'
+import { FcGoogle } from "react-icons/fc";
+
+
+const Home = () => {
+    const handleLogin=async (token)=>{
+    try{
+      const {data}=await api.post("/auth/login",{token})
+      console.log(data)
+    }catch(error){
+      console.log(error)
+    }
+  }
+  
+  const googleLogin=async ()=>{
+   const data= await signInWithPopup(auth,googleProvider)
+   const token=await data.user.getIdToken()
+   console.log(token)
+   await handleLogin(token)
+   console.log(data)
+  }
+
+  return (
+    <div className='h-screen flex bg-[#0d0f14] text-white overflow-hidden'>
+      <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm'>
+        <div className='w-[340px] bg-[#13151c] border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5'>
+        <div className='flex flex-col gap-1'>
+            <h2 className='text-[17px] font-semibold text-slate-100 tracking-tight'>Welcome to AskAI</h2>
+            <p className='text-[13px] text-slate-500'>Please login to continue using the app.</p>
+        </div>
+
+        <button className="w-full flex items-center justify-center gap-3 py-[11px] rounded-xl text-sm font-medium text-black bg-white hover:bg-gray-100 active:bg-gray-200 border border-gray-300 shadow-lg hover:shadow-xl transition-all duration-150 cursor-pointer" onClick={googleLogin}>
+            <FcGoogle size={15} />
+            Continue with Google
+        </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
